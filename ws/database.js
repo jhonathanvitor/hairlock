bcf13d44c80884a43adac3f3e1e6aaeced199fd6
jhonathan vitor <jhonathanvitor@gmail.com>
2022-11-09 15:26:49 -0400

    mongodb string connection

--- ws/database.js
+++ ws/database.js
@@ -1,11 +1,9 @@
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 
-const URI = process.env.STRING_URL;
-
 dotenv.config();
 
 mongoose
-  .connect(process.env.STRING_URL)
+  .connect(process.env.MONGODB_URL)
   .then(() => console.log('DB is Up!'))
   .catch((err) => console.log(err));
